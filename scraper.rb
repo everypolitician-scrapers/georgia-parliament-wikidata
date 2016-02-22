3f2b1524bd45416d0d80fc1424fc50991aab3b76
Tony Bowden <tony@tmtm.com>
2016-02-22 17:04:13 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'pry'
-require 'rest-client'
 
-@pages = [
-  'კატეგორია:საქართველოს_მე-8_მოწვევის_პარლამენტის_წევრები',
-]
-
-ids = @pages.map { |c| WikiData::Category.new(c, 'ka').wikidata_ids }.flatten.uniq
-ids.each_with_index do |id, i|
-  puts i if (i % 50).zero?
-  data = WikiData::Fetcher.new(id: id).data('ka') or next
-  ScraperWiki.save_sqlite([:id], data) rescue binding.pry
-end
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = WikiData::Category.new( 'კატეგორია:საქართველოს_მე-8_მოწვევის_პარლამენტის_წევრები', 'ka').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { ka: names }, output: false)
+warn EveryPolitician::Wikidata.notify_rebuilder
