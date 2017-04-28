a8d462842eacdd68de904ed8903d31b369f3d933
Tony Bowden <tony@tmtm.com>
2017-04-28 09:19:42 +0100

    Also get via English Wikipedia

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,6 @@
 
 require 'wikidata/fetcher'
 
-names = WikiData::Category.new( 'კატეგორია:საქართველოს_მე-8_მოწვევის_პარლამენტის_წევრები', 'ka').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { ka: names }, output: false)
+ka = WikiData::Category.new( 'კატეგორია:საქართველოს_მე-8_მოწვევის_პარლამენტის_წევრები', 'ka').member_titles
+en = WikiData::Category.new( 'Category:Members of the Parliament of Georgia', 'en').member_titles
+EveryPolitician::Wikidata.scrape_wikidata(names: { ka: ka, en: en })
