3ca21ae668b5cc3b375c3a67a90b3192d35ecf6b
Tony Bowden <tony@tmtm.com>
2018-10-26 06:47:39 +0100

    Update for changed ka Category

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,7 @@
 
 require 'wikidata/fetcher'
 
-ka = WikiData::Category.new( 'კატეგორია:საქართველოს_მე-8_მოწვევის_პარლამენტის_წევრები', 'ka').member_titles
+ka = WikiData::Category.new( 'კატეგორია:საქართველოს VIII მოწვევის პარლამენტის წევრები', 'ka').member_titles
 en = WikiData::Category.new( 'Category:Members of the Parliament of Georgia', 'en').member_titles
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { ka: ka, en: en })
